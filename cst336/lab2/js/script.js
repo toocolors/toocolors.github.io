b85dcb822a74769c84a13c633ce98701e2361ae7
toocolors <188366890+toocolors@users.noreply.github.com>
2025-11-02 11:30:42 -0800

    Added initializeGame function to script.js

--- cst336/lab2/js/script.js
+++ cst336/lab2/js/script.js
@@ -1,3 +1,18 @@
 // Global variables
-let randomNumber = Math.floor(Math.random() * 99) + 1;
-console.log(randomNumber);
+let randomNumber;
+let attempts = 0;
+
+/**
+ * Initializes the game.
+ */
+function initializeGame() {
+    // Get random number
+    randomNumber = Math.floor(Math.random() * 99) + 1;
+    console.log(`randomNumber: ${randomNumber}`);
+
+    // Hide reset button
+    document.querySelector("#resetBtn").computedStyleMap.display = "none";
+
+    // Set focus to textbox
+    document.querySelector("#playerGuess").focus();
+}
