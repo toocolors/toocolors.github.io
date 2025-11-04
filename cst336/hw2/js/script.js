cd6a047d1295cca181d8c7c01eb5144c62f08076
toocolors <188366890+toocolors@users.noreply.github.com>
2025-11-04 07:25:27 -0800

    Finished step 8 of HW2 tutorial.

--- cst336/hw2/js/script.js
+++ cst336/hw2/js/script.js
@@ -1,3 +1,6 @@
+// Global variables
+let score = 0;
+
 // Add event listeners
 document.querySelector("button").addEventListener("click", gradeQuiz);
 
@@ -9,7 +12,7 @@ function gradeQuiz() {
     }
 
     // variables
-    let score = 0;
+    score = 0;
     let q1Response = document.querySelector("#q1").value.toLowerCase();
     let q2Response = document.querySelector("#q2").value;
     console.log(q2Response);
@@ -17,31 +20,28 @@ function gradeQuiz() {
 
     // Grading Question 1
     if (q1Response == "sacramento") {
-        document.querySelector("#q1Feedback").innerHTML = "Correct!";
-        document.querySelector("#q1Feedback").className = "bg-success text-white";
-        document.querySelector("#markImg1").innerHTML = "<img src='img/checkmark.png' alt=Checkmark'>"
-        score += 20;
+        rightAnswer(1);
     } else {
-        document.querySelector("#q1Feedback").innerHTML = "Incorrect!";
-        document.querySelector("#q1Feedback").className = "bg-warning text-white";
-        document.querySelector("#markImg1").innerHTML = "<img src='img/xmark.png' alt=Checkmark'>"
+        wrongAnswer(1)
     }
 
     // Grading Question 2
     if(q2Response == "mo") {
-        document.querySelector("#q2Feedback").innerHTML = "Correct!";
-        document.querySelector("#q2Feedback").className = "bg-success text-white";
-        document.querySelector("#markImg2").innerHTML = "<img src='img/checkmark.png' alt=Checkmark'>"
-        score += 20;
+        rightAnswer(2);
     } else {
-        document.querySelector("#q2Feedback").innerHTML = "Incorrect!";
-        document.querySelector("#q2Feedback").className = "bg-warning text-white";
-        document.querySelector("#markImg2").innerHTML = "<img src='img/xmark.png' alt=Checkmark'>"
+        wrongAnswer(2);
     }
 
     // Show total score
     document.querySelector("#totalScore").innerHTML = `Total Score: ${score}`;
-}//gradeQuiz
+} //gradeQuiz
+
+function rightAnswer(index) {
+    document.querySelector(`#q${index}Feedback`).innerHTML = "Correct!";
+    document.querySelector(`#q${index}Feedback`).className = "bg-success text-white";
+    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/checkmark.png' alt=Checkmark'>"
+    score += 20;
+} //rightAnswer
 
 function isFormValid() {
     let isValid = true;
@@ -51,3 +51,9 @@ function isFormValid() {
     }
     return isValid;
 } //isFormValid
+
+function wrongAnswer(index) {
+    document.querySelector(`#q${index}Feedback`).innerHTML = "Incorrect!";
+    document.querySelector(`#q${index}Feedback`).className = "bg-warning text-white";
+    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/xmark.png' alt=Checkmark'>"
+} //wrongAnswer
