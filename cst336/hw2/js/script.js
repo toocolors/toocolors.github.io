29c82599f8e6043d6de52510e825f346d9bbdd3a
toocolors <188366890+toocolors@users.noreply.github.com>
2025-11-04 07:01:10 -0800

    Finished part 6 of HW2 tutorial.

--- cst336/hw2/js/script.js
+++ cst336/hw2/js/script.js
@@ -3,20 +3,35 @@ document.querySelector("button").addEventListener("click", gradeQuiz);
 
 function gradeQuiz() {
     console.log("Grading quiz...");
-    document.querySelector("#q1Feedback").innerHTML = "";
+    document.querySelector("#q1Validation").innerHTML = "";
     if (!isFormValid()) {
         return;
     }
 
-    let q1Response = document.querySelector("#q1").value;
+    // variables
+    let score = 0;
+    let q1Response = document.querySelector("#q1").value.toLowerCase();
     console.log(q1Response);
-}
+
+    // Grading Question 1
+    if (q1Response == "sacramento") {
+        document.querySelector("#q1Feedback").innerHTML = "Correct!";
+        document.querySelector("#q1Feedback").className = "bg-success text-white";
+        document.querySelector("#markImg1").innerHTML = "<img src='img/checkmark.png' alt=Checkmark'>"
+        score += 20;
+    } else {
+        document.querySelector("#q1Feedback").innerHTML = "Inorrect!";
+        document.querySelector("#q1Feedback").className = "bg-warning text-white";
+        document.querySelector("#markImg1").innerHTML = "<img src='img/xmark.png' alt=Checkmark'>"
+    }
+    document.querySelector("#totalScore").innerHTML = `Total Score: ${score}`;
+}//gradeQuiz
 
 function isFormValid() {
     let isValid = true;
     if(document.querySelector("#q1").value == "") {
         isValid = false;
-        document.querySelector("#q1Feedback").innerHTML = "Question 1 was not answered";
+        document.querySelector("#q1Validation").innerHTML = "Question 1 was not answered";
     }
     return isValid;
 } //isFormValid
