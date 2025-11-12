baaa3cdf1bdb616a509ab870061b9e9771bf1d71
toocolors <188366890+toocolors@users.noreply.github.com>
2025-11-12 07:23:11 -0800

    HW3 stores and checks expiration date for pokemon list. Refactored methods.

--- cst336/hw3/js/script.js
+++ cst336/hw3/js/script.js
@@ -6,30 +6,75 @@ let pokemonList = []
 // Event Listeners
 
 // Call Functions
-updatePokemonList()
+checkExpDate();
 
 // FUNCTIONS
 
-async function updatePokemonList() {
-    // Check for Pokemon List in local storage
-    let data = localStorage.getItem("pokemonList");
-    let json;
-    if (data != null) {
-        try {
-            json = await JSON.parse(data);
-            pokemonList = json;
-            return;
-        } catch (err) {
-            console.error("Failed to parse pokemon list JSON", err);
-        }
+function checkExpDate() {
+    // Get expiration date
+    console.log("Getting expiration date from local storage...");
+    let isDateValid = true;
+    let expDate = Date.parse(localStorage.getItem("pokemonListExpirationDate"));
+    let currentDate = new Date();
+    
+    // Check for expiration date in local storage, compare it to current date
+    if (expDate == null) {
+        console.log("Expiration date not found.");
+        isDateValid = false;
+    } else if (expDate < currentDate) {
+        console.log("Pokemon list has expired.");
+        isDateValid = false;
     }
 
+    // Check for expiration date validity
+    if(!isDateValid) {
+        updatePokemonList();
+        return;
+    } else {
+        console.log("Expiration date is valid!");
+        getPokemonList();
+    }
+}
+
+async function getPokemonList() {
+    // Check for Pokemon List in local storage
+    console.log("Getting pokemon list from local storage...")
+    let data = localStorage.getItem("pokemonList");
+    let json;
+    if (data == null) {
+        updatePokemonList();
+        return;
+    }
+
+    // Parse pokemon list from local storage text
+    try {
+        json = await JSON.parse(data);
+        pokemonList = json;
+        console.log("Got pokemon list from local storage!")
+    } catch (err) {
+        console.error("Failed to parse pokemon list JSON", err);
+        updatePokemonList();
+    }
+}
+
+function updateExpDate() {
+    let newDate = new Date()
+    newDate = new Date(newDate.setMonth(newDate.getMonth() + 1));
+    localStorage.setItem("pokemonListExpirationDate", newDate.toString());
+}
+
+async function updatePokemonList() {
     // Get Pokemon list from API
+    console.log("Getting pokemon list from PokeAPI...")
     let url = `${apiURL}?limit=100000`
-    data = await fetch(url);
-    json = await data.json();
-    pokemonList = json;
+    let response = await fetch(url);
+    let data = await response.json();
+    pokemonList = data;
 
     // Save Pokemon list in local storage
-    localStorage.setItem("pokemonList", JSON.stringify(json));
+    console.log("Saving pokemon list to local storage...");
+    localStorage.setItem("pokemonList", JSON.stringify(data));
+
+    // Save new expiration date
+    updateExpDate();
 }
